9130ce086636ce6bfe8cd9a847a08c5670c68cfc
Akshaye <akshaye.singhi@m2pfintech.com>
2023-10-16 13:19:19 +0530

    secrets removed

--- src/main/java/com/example/testprunesecrets/DataSecurityService.java
+++ src/main/java/com/example/testprunesecrets/DataSecurityService.java
@@ -3,7 +3,7 @@ package com.example.testprunesecrets;
 public class DataSecurityService {
 
     public void main (String[] args) throws Exception {
-        DataSecurityClient.dryRun();
+        System.out.println("HELLO WORLD");
     }
 
 }
