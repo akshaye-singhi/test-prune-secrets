ee4d13012b3048f9695d8ca612b986bfcbbab1bb
Akshaye <akshaye.singhi@m2pfintech.com>
2023-10-16 13:18:07 +0530

    with secrets

---
+++ src/main/java/com/example/testprunesecrets/DataSecurityService.java
@@ -0,0 +1,9 @@
+package com.example.testprunesecrets;
+
+public class DataSecurityService {
+
+    public void main (String[] args) throws Exception {
+        DataSecurityClient.dryRun();
+    }
+
+}
